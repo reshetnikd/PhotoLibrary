7ebf7d1c8b494f6820d80716fa6579f615bd18a0
reshetnikd <dmitry.reshetnik@icloud.com>
2020-04-20 12:42:16 +0300

    Add basic UI and data structure

---
+++ PhotoLibrary/Photo.swift
@@ -0,0 +1,14 @@
+//
+//  Photo.swift
+//  PhotoLibrary
+//
+//  Created by Dmitry Reshetnik on 20.04.2020.
+//  Copyright © 2020 Dmitry Reshetnik. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Codable {
+    var name: String
+    var image: String
+}
--- PhotoLibrary/ViewController.swift
+++ PhotoLibrary/ViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var photos = [Photo]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
